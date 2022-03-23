9cc31ba25adc22ea8930861c9cca3836ec79a7eb
david <david@PAVILION>
2022-03-23 07:49:02 -0500

    Client jwt configuration

--- Movies.Client/Startup.cs
+++ Movies.Client/Startup.cs
@@ -32,11 +32,24 @@ namespace Movies.Client
 
             services.AddScoped<IMovieApiService, MovieApiService>();
 
-            services.AddAuthentication(options => {
+            services.AddAuthentication(options =>
+            {
                 options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
                 options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
             })
-            
+            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+             {
+                 options.Authority = "https://localhost:5005";
+                 options.ClientId = "movies_mvc_client";
+                 options.ResponseType = "code";
+
+                 options.Scope.Add("openid");
+                 options.Scope.Add("profile");
+
+                 options.SaveTokens = true;
+                 options.GetClaimsFromUserInfoEndpoint = true;
+             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
